9077fa3f5f670d46970cc18a5a0fe0e1b40c4a4b
tianfengluyao <15002336757@139.com>
2018-08-01 08:57:09 +0800

    project init

--- src/main/java/com/maple/house/client2.java
+++ src/main/java/com/maple/house/client2.java
@@ -4,11 +4,11 @@ package com.maple.house;
 public class client2 {
 
     public static void main(String[] args) {
-        CalculateProfit fixationMontly = new CalculateProfit(5.8,0.08,15);
-        System.out.println(fixationMontly.getAmountMoney());
+        CalculateProfit fixationMontly = new CalculateProfit(300000,0.11,30);
+        System.out.println("首付总收益  ："+ (int)fixationMontly.getAmountMoney());
 
         //月定投总收益
-        FixationMontly fixationMontly1 = new FixationMontly(5000,0.08,30);
-        System.out.println("月定投总收益："+fixationMontly1.getTotal());
+        FixationMontly fixationMontly1 = new FixationMontly(1000,0.08,30);
+        System.out.println("月定投总收益："+(int)fixationMontly1.getTotal());
     }
 }
